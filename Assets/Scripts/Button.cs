fcc235d19665abd9ab4e9b94eaa3f5309fce1dcd
Joshua <joshuadjacoby@gmail.com>
2016-05-18 00:25:27 -0700

    Evidence Notebook has clickable buttons that lead to their own scenes...art to be added later

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void showEvidence() {
+        SceneManager.LoadScene((EventSystem.current.currentSelectedGameObject).name);
+    }
+
+    public void returnToNotebook() {
+        SceneManager.LoadScene("Evidence Notebook");
+    }
+}
