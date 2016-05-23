01e02c1b9ba25dd670b46a6eb0114b0568d2a2d7
Joshua <joshuadjacoby@gmail.com>
2016-05-23 15:31:28 -0700

    Added Evidence file art

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -3,23 +3,23 @@ using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class Button : MonoBehaviour {
+    public class Button : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        // Use this for initialization
+        void Start() {
 
-    public void showEvidence() {
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public void showEvidence() {
         SceneManager.LoadScene((EventSystem.current.currentSelectedGameObject).name);
     }
 
-    public void returnToNotebook() {
-        SceneManager.LoadScene("Evidence Notebook");
+        public void returnToEvidence() {
+            SceneManager.LoadScene("Evidence");
+        }
     }
-}
