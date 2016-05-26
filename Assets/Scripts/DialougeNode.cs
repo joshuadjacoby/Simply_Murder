42d01d4a09f8f09e293ef8608ec69671fd0d9b84
Brendon <btelliar@uci.edu>
2016-05-25 17:19:55 -0700

    Finalized node based system

---
+++ Assets/Scripts/DialougeNode.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class DialougeNode : MonoBehaviour {
+    //[HideInInspector]
+    public Text textbox;
+    //private textobject;
+    public List<string> dialogueList = new List<string>();
+    //public GameObject leftSprite, rightSprite;
+    public GameObject nextGameObject;
+    private int iterator = 0;
+    //public GameObject nextNode;
+
+    // Use this for initialization
+    void Start ()
+    {
+        textbox = GameObject.Find("Words").GetComponent<Text>();
+        textbox.text = dialogueList[iterator];
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    void NextText(string text)
+    {
+        textbox.text = text;
+    }
+    void OnMouseDown()
+    {
+        Debug.Log(iterator + "count" + dialogueList.Count);
+        
+        if (iterator == dialogueList.Count)
+        {
+           // Destroy(leftSprite);
+            //Destroy(rightSprite);
+            Instantiate(nextGameObject, this.gameObject.transform.position, this.gameObject.transform.rotation);
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            NextText(dialogueList[iterator++]);
+            Debug.Log(iterator);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public int Jury;
+    public Text textbox;
+    public string currentEvidence;
+    public int dialougeState;
+    public Dictionary<string, List<string>> evidence = new Dictionary<string, List<string>>(); 
+    
+	void Start ()
+    {
+        dialougeState = 0;
+        Jury = 3;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+}
